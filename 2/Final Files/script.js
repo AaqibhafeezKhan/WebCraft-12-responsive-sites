00b3e36a30e709acec1a37f9fddd3275dd3f6fe2
AaqibhafeezKhan <aaqibhafeezkhan@gmail.com>
2026-04-21 22:31:32 +0530

    feat: add second responsive design implementation

---
+++ 2/Final Files/script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.c_43x8kx').forEach(btn => {
+    btn.addEventListener('click', () => {
+        btn.classList.toggle('c_wpx91h')
+        btn.nextElementSibling.classList.toggle('c_wpx91h')
+    })
+})
