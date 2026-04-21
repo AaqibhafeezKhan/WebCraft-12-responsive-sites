e3c413da14e4f6c5fc678cecd0c38699bbb85c80
AaqibhafeezKhan <aaqibhafeezkhan@gmail.com>
2026-04-21 22:31:42 +0530

    feat: finish fifth responsive site implementation

---
+++ 5/Final Files/script.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+  setTimeout(() => {
+    document.querySelector("body").classList.add("c_len4zv");
+  }, 4000);
+};
+
+document.querySelector(".c_q4hkf3").addEventListener("click", () => {
+  document.querySelector(".c_rlui8o").classList.toggle("c_1jphiq");
+});
+
+document.querySelector(".c_qg1nz0").addEventListener("click", () => {
+  document.querySelector("html").style.scrollBehavior = "smooth";
+  setTimeout(() => {
+    document.querySelector("html").style.scrollBehavior = "unset";
+  }, 1000);
+});
